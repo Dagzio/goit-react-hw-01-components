ebee8705f689dc487e478ac0069ac0d7a18390f1
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-04-28 16:51:52 +0300

    update FriendList files

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,9 +2,11 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
 
 
-const FriendList = ( friends ) => {
+const FriendList = ( {friends} ) => {
   return <ul className="friend-list">
-    <FriendListItem />
+    {friends.map(friend => {
+      return <FriendListItem />
+    })}
   </ul>
 };
 
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,22 +1,21 @@
 import PropTypes from 'prop-types'
 
 
-const FriendListItem = ({ friends}) => {
-    return friends.map(({avatar, name, isOnline, id}) => {
-        return <li class="item" key={id}>
-            <span class="status" type={isOnline}></span>
-            <img class="avatar" src={avatar} alt="User avatar" width="48" />
-            <p class="name">{name}</p>
-        </li>
-    });
+const FriendListItem = ({ avatar, name, isOnline, id }) => {
+   return <li className="item" key={id}>
+        <span className="status" type={isOnline}></span>
+        <img className="avatar" src={avatar} alt="User avatar" width="48" />
+        <p className="name">{name}</p>
+    </li>
 };
+
 
 FriendListItem.propTypes = {
     friends: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.boolean.isRequired,
+        id: PropTypes.string,
+        avatar: PropTypes.string,
+        name: PropTypes.string,
+        isOnline: PropTypes.boolean
     }))
 };
 
